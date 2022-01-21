efd70d036aeb88c54ef40ec7fd60d597f195f695
Denzel <denzerur@gmail.com>
2022-01-21 15:22:52 +0800

    Update the data in taskItemList

--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -3,20 +3,25 @@ import Task from "./Task";
 
 function TasksList() {
 
-    const taskItemsList = [
+    const [taskItemsList, setTaskItemsList] = useState ([
 
         "Follow Edukasyon.ph on Facebook",
         "Follow AWS Siklab Pilipinas on Facebook",
         "Follow Zuitt Coding Bootcamp on Facebook"
 
-    ];
+    ]);
 
-    const [taskValue, setTaskValue] = useState("Just another task");
-    console.log("taskValue: ", taskValue);
+    const [taskValue, setTaskValue] = useState("");
+ 
 
     const inputChangeHandler = (e) => {
         
         setTaskValue(e.target.value)
+    };
+
+    const addTaskHandler = () => {
+        setTaskItemsList([taskValue, ...taskItemsList]);
+        setTaskValue("");
     };
 
     return(
@@ -25,6 +30,8 @@ function TasksList() {
              className="task-input"
              placeholder="Create a new task"
              onChange={inputChangeHandler}
+             onBlur={addTaskHandler}
+             value={taskValue}
              />
         <ul>
             {taskItemsList.map((task, index) => {
